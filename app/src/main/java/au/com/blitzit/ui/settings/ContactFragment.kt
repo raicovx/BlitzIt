c21fc02ad62a0024bbbde1f883edd71caa3ad8fe
Garion Hystek <garionhystek@gmail.com>
2021-05-27 09:30:29 +1000

    -Added deep linking to ContactFragment.kt

--- app/src/main/java/au/com/blitzit/ui/settings/ContactFragment.kt
+++ app/src/main/java/au/com/blitzit/ui/settings/ContactFragment.kt
@@ -1,13 +1,16 @@
 package au.com.blitzit.ui.settings
 
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import au.com.blitzit.R
+import org.w3c.dom.Text
 
 class ContactFragment : Fragment()
 {
@@ -33,6 +36,8 @@ class ContactFragment : Fragment()
             this.findNavController().navigate(ContactFragmentDirections.actionContactFragmentToBookingFragment())
         }
         bookButton.visibility = View.GONE
+
+        view.findViewById<TextView>(R.id.contact_us_web_link).movementMethod = LinkMovementMethod.getInstance()
 
         return view
     }
