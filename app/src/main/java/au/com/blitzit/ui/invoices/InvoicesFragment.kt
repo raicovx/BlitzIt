51638dd732cc8ddb8ff380d618291ccab787b0b4
Garion Hystek <garionhystek@gmail.com>
2021-05-27 14:24:39 +1000

    -Minor warning fixes

--- app/src/main/java/au/com/blitzit/ui/invoices/InvoicesFragment.kt
+++ app/src/main/java/au/com/blitzit/ui/invoices/InvoicesFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-import androidx.core.view.contains
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
