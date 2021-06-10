14ac4b7c4aaf867fa240b27e4f277b099e50c1f6
Garion Hystek <garionhystek@gmail.com>
2021-06-10 20:02:52 +1000

    -Version 1.0.3 -Fixed missing link in about app (privacy link)

--- app/src/main/java/au/com/blitzit/ui/settings/AboutAppFragment.kt
+++ app/src/main/java/au/com/blitzit/ui/settings/AboutAppFragment.kt
@@ -1,10 +1,13 @@
 package au.com.blitzit.ui.settings
 
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.text.method.MovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import au.com.blitzit.R
@@ -24,6 +27,9 @@ class AboutAppFragment : Fragment()
 
         backButton = view.findViewById(R.id.about_app_back_button)
         backButton.setOnClickListener { this.findNavController().navigate(AboutAppFragmentDirections.actionAboutAppFragmentToSettingsFragment()) }
+
+        val pTV = view.findViewById<TextView>(R.id.privacy_link)
+        pTV.movementMethod = LinkMovementMethod.getInstance()
 
         return view
     }
